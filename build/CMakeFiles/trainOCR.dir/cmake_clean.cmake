cd139b10ee506c65dd9b6592a5dfba71ea6733f6
mxq <1529806383@qq.com>
2018-07-27 21:08:49 +0800

    some changes

---
+++ build/CMakeFiles/trainOCR.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trainOCR.dir/trainOCR.cpp.o"
+  "CMakeFiles/trainOCR.dir/OCR.cpp.o"
+  "../bin/trainOCR.pdb"
+  "../bin/trainOCR"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/trainOCR.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
